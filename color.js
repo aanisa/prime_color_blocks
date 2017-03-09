11910d43c2ff57a37ad90f5b1c7e27e325fd9223
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-09 17:22:45 -0600

    blocks delete when clicked, counter reflects change

--- color.js
+++ color.js
@@ -1,42 +1,84 @@
-  var countR = 1;
-  var countY = 1;
-  var countB = 1;
-  var countG = 1;
+  var countR = 0;
+  var countY = 0;
+  var countB = 0;
+  var countG = 0;
 
 
 
-$(document).ready(function() {
-  $('.addBlocks').append('<div class="blocks" id="red"></div>');
-  $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
-  $('.addBlocks').append('<div class="blocks" id="blue"></div>');
-  $('.addBlocks').append('<div class="blocks" id="green"></div>');
+  $(document).ready(function() {
+  
+      $('.addBlocks').on('click', '#red', function() {
+          $(this).remove();
+          countR -= 1;
+          $('.RedCount').text("Red Count: " + countR);
+      });
 
-  $('.redButton').on("click", function() { //use .on not .click
+      $('.redButton').on("click", function() { //use .on not .click
+          $('.addBlocks').append('<div class="blocks" id="red"></div>');
+          countR += 1;
+          $('.RedCount').text("Red Count: " + countR);
+      });
+
+      $('.addBlocks').on('click', '#yellow', function() {
+          $(this).remove();
+          countY -= 1;
+          $('.YellowCount').text("Yellow Count: " + countY);
+      });
+
+      $('.yellowButton').on("click", function() {
+          $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
+          countY += 1;
+          $('.YellowCount').text("Yellow Count: " + countY);
+      });
+
+      $('.addBlocks').on('click', '#blue', function() {
+          $(this).remove();
+          countB -= 1;
+          $('.BlueCount').text("Blue Count: " + countB);
+      });
+
+      $('.blueButton').on("click", function() {
+          $('.addBlocks').append('<div class="blocks" id="blue"></div>');
+          countB += 1;
+          $('.BlueCount').text("Blue Count: " + countB);
+      });
+
+      $('.addBlocks').on('click', '#green', function() {
+          $(this).remove();
+          countG -= 1;
+          $('.GreenCount').text("Green Count: " + countG);
+      });
+
+      $('.greenButton').on("click", function() {
+          $('.addBlocks').append('<div class="blocks" id="green"></div>');
+          countG += 1;
+          $('.GreenCount').text("Green Count: " + countG);
+      });
+
+  });
+
+
+
+
+
+
+  /* Alternative Method
+  var countR = 0;
+
+  $(document).ready(function() {
+
+    $('.redButton').on("click", function() {
+      countR+=1;
+      $('.RedCount').text("Red Count: " + countR);
+
+      addRedClock();
+
+    });
+    $(".blocks").on("click", "#red", function(){
+        $(this).remove();
+  });
+
+  function addRedBlock() {
     $('.addBlocks').append('<div class="blocks" id="red"></div>');
-    countR+=1;
-    $('.RedCount').text("Red Count: " + countR);
-  });
-
-  $('.yellowButton').on("click", function() {
-    $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
-    countY+=1;
-    $('.YellowCount').text("Yellow Count: " + countY);
-  });
-
-  $('.blueButton').on("click", function() {
-    $('.addBlocks').append('<div class="blocks" id="blue"></div>');
-    countB+=1;
-    $('.BlueCount').text("Blue Count: " + countB);
-  });
-
-  $('.greenButton').on("click", function() {
-    $('.addBlocks').append('<div class="blocks" id="green"></div>');
-    countG+=1;
-    $('.GreenCount').text("Green Count: " + countG);
-  });
-
-});
-
-//hard mode need block to dissappear when clicked on an counter to reflect that change
-//use prepend()
-//prework video about clicks
+  }
+  */
