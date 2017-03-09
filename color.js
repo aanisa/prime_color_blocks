40eea4f4a32de7f8f77bd105f3ace3665ae0b26e
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-09 15:39:06 -0600

    Message here

--- color.js
+++ color.js
@@ -1,19 +1,44 @@
+  var countR = 1;
+  var countY = 1;
+  var countB = 1;
+  var countG = 1;
+
+
+
 $(document).ready(function() {
   $('.addBlocks').append('<div class="blocks" id="red"></div>');
   $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
   $('.addBlocks').append('<div class="blocks" id="blue"></div>');
   $('.addBlocks').append('<div class="blocks" id="green"></div>');
 
-  $('.redButton').click(function() {
+  $('.redButton').on("click", function() { //use .on not .click
     $('.addBlocks').append('<div class="blocks" id="red"></div>');
+    countR+=1;
+    $('.RedCount').text("Red Count: " + countR);
   });
-  $('.yellowButton').click(function() {
+
+  $('.yellowButton').on("click", function() {
     $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
+    countY+=1;
+    $('.YellowCount').text("Yellow Count: " + countY);
   });
-  $('.blueButton').click(function() {
+
+  $('.blueButton').on("click", function() {
     $('.addBlocks').append('<div class="blocks" id="blue"></div>');
+    countB+=1;
+    $('.BlueCount').text("Blue Count: " + countB);
   });
-  $('.greenButton').click(function() {
+
+  $('.greenButton').on("click", function() {
     $('.addBlocks').append('<div class="blocks" id="green"></div>');
+    countG+=1;
+    $('.GreenCount').text("Green Count: " + countG);
   });
+
+
+
 });
+
+//hard mode need block to dissappear when clicked on an counter to reflect that change
+//use prepend()
+//prework video about clicks
