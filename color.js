977354733925b92777fed20432a6441c950f3771
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-08 21:44:23 -0600

    add blocks using jquery

--- color.js
+++ color.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-
+  $('.addBlocks').append('<div class="blocks" id="red"></div>');
+  $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
+  $('.addBlocks').append('<div class="blocks" id="blue"></div>');
+  $('.addBlocks').append('<div class="blocks" id="green"></div>');
 
 });
