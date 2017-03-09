8d6faee6ea8de2b8f65b6425000a91c3bf5cc631
Anisa Abdulkadir <anisa.ali7@gmail.com>
2017-03-08 22:10:29 -0600

    add corresponding block when button is clicked

--- color.js
+++ color.js
@@ -4,6 +4,16 @@ $(document).ready(function() {
   $('.addBlocks').append('<div class="blocks" id="blue"></div>');
   $('.addBlocks').append('<div class="blocks" id="green"></div>');
 
-  $('.button').onClick(function() {}
-    //when click button, add block of respective color. count
+  $('.redButton').click(function() {
+    $('.addBlocks').append('<div class="blocks" id="red"></div>');
+  });
+  $('.yellowButton').click(function() {
+    $('.addBlocks').append('<div class="blocks" id="yellow"></div>');
+  });
+  $('.blueButton').click(function() {
+    $('.addBlocks').append('<div class="blocks" id="blue"></div>');
+  });
+  $('.greenButton').click(function() {
+    $('.addBlocks').append('<div class="blocks" id="green"></div>');
+  });
 });
